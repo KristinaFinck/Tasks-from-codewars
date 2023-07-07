7e01b508a12bc26e9069a9cc59e2db23f0f40d67
KristinaFinck <kristana.finck83@gmail.com>
2023-07-07 11:55:21 +0300

    codewars 8 - function

---
+++ helloworld.js
@@ -0,0 +1,7 @@
+/* Training JS #1: create your first JS function and print "Hello World!" */
+   function helloWorld() {
+    var somewords = "Hello World";
+    console.log(somewords);
+  }
+  
+   helloWorld();
