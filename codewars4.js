78ba3299fc784f0063ed48a00f5cdd24b8774846
KristinaFinck <kristana.finck83@gmail.com>
2023-07-17 17:24:56 +0300

    The Wide-Mouthed frog!

---
+++ codewars4.js
@@ -0,0 +1,11 @@
+function mouthSize(animal) {
+    // code here
+     let mouth_size;
+    if(animal == 'alligator') {
+      mouth_size = 'small'
+    } else {
+      mouth_size = 'wide';
+    }
+    return mouth_size;
+  }
+  //https://www.codewars.com/kata/57ec8bd8f670e9a47a000f89/train/javascript
