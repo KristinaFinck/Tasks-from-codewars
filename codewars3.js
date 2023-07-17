8aa40295e75b8539b68e795cd13148c70172f37a
KristinaFinck <kristana.finck83@gmail.com>
2023-07-17 15:49:58 +0300

    задача на логику

---
+++ codewars3.js
@@ -0,0 +1,8 @@
+function setAlarm(employed, vacation){
+    if (employed == true && vacation == false && employed !== undefined && vacation !== undefined) {
+      return true;
+    } else {
+      return false
+    }
+  }
+/*  https://www.codewars.com/kata/568dcc3c7f12767a62000038/train/javascript */
