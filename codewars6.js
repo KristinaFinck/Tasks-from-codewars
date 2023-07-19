18d9bb80688c06d787a7d135c718416bf434df54
KristinaFinck <kristana.finck83@gmail.com>
2023-07-19 11:28:34 +0300

    Simple multiplication

---
+++ codewars6.js
@@ -0,0 +1,10 @@
+//https://www.codewars.com/kata/583710ccaa6717322c000105/train/javascript
+function simpleMultiplication(number) {
+    // your code........
+  if(number%2 === 0) {
+    number *=8;
+  } else {
+    number *= 9;
+  }
+    return number
+}
